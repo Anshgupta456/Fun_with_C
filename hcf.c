d86a4e783565ae1f78f013fd564ad2964c14151e
Anshika <anshikagupta45612@gmail.com>
2022-07-12 09:49:02 +0530

    Code to find hcf of two numbers

---
+++ hcf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num1, num2, i, gcd;
+    printf("Enter the numbers = ");
+    scanf("%d %d", &num1, &num2);
+    for(i = 1; i < num1 || i < num2; i++){
+        if(num1 % i == 0 && num2 % i == 0){
+            gcd = i;
+        }
+    }
+    printf("Highest common factor of %d and %d = %d", num1, num2, gcd);
+    return 0;
+} 
